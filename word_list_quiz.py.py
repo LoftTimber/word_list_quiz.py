abf51cb39d57164b0f3668d38cb3b33748bd07bc
LoftTimber <31486781+LoftTimber@users.noreply.github.com>
2017-12-05 14:50:58 -0500

    Add files via upload

--- word_list_quiz.py.py
+++ word_list_quiz.py.py
@@ -1,13 +1,6 @@
 with open('words_alpha.txt', 'r') as f:
     words = f.read().splitlines()
 
-def ending_letter_s():
-    count = 0
-    for w in words:
-        if w[-1] == 's':
-            count += 1
-    print(count)
-ending_letter_s()
 
 def word_count():
     count = 0
@@ -24,3 +17,27 @@ def total_number_of_characters():
         count += len(w)
     print(count)
 total_number_of_characters()
+
+def words_without_an_e():
+    count = 0
+    for w in words:
+        if 'e' not in w:
+            count += 1
+    print(count)
+words_without_an_e()
+
+def begin_and_end_with_same_letter():
+    count = 0
+    for w in words:
+        if w[0] == w[-1]:
+            count += 1
+    print(count)
+begin_and_end_with_same_letter()
+
+def exactly_3_as():
+    count = 0
+    for w in words:
+        if  in w:
+            count += 1
+    print(count)
+exactly_3_as()
